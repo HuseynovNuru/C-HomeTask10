a9a01abb5c42dfea3fa5312d8046d795fd6df961
HuseynovNuru <121167956+HuseynovNuru@users.noreply.github.com>
2023-01-31 21:30:47 +0400

    Add files via upload

---
+++ CHomeTask10.2/CHomeTask10.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CHomeTask10._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student.CheckNo();
+
+            Student std1 = new Student();
+            Student std2 = new Student();
+            Student std3 = new Student();
+            Student std4 = new Student();
+
+            std1.Name = "Nuru";
+            std2.Name = "Vuqar";
+            std3.Name = "Elvin";
+            std4.Name = "Samir";
+
+            std1.GroupNo = "P231";
+            std2.GroupNo = "P231";
+            std3.GroupNo = "P231";
+            std4.GroupNo = "P231";
+
+            Console.WriteLine(std1.Name);
+            Console.WriteLine(std4.No);
+            Console.WriteLine(std1.GroupNo);
+            Console.WriteLine(std1.CheckGroupNo("P231"));
+        }
+    }
+}
---
+++ CHomeTask10.2/CHomeTask10.2/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CHomeTask10._2
+{
+    internal class Student
+    {
+        private static int _totalCount;
+        public Student()
+        {
+            _totalCount++;
+            No = _totalCount;
+        }
+
+        public string Name;
+        public int No;
+        public string GroupNo;
+
+        static public void CheckNo()
+        {
+        }
+
+        public bool CheckGroupNo(string groupNo)
+        {
+            if (groupNo.Length == 4 && char.IsUpper(groupNo[0]))
+            {
+                for (int i = 1; i < groupNo.Length; i++)
+                {
+                    if (!char.IsDigit(groupNo[i]))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
